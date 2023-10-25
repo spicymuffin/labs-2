fa557d031fdccc4ce9f2eee2ce0e75c9ee7b0d7c
spicymuffin <luigicussigh59@gmail.com>
2023-10-25 12:08:57 +0900

    midterms!

---
+++ class/10-22-2023.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 123;
+	int b = 2;
+	int c = 0;
+	int d = 0;
+    float fl = 0;
+    fl = a / b;
+    fl += 0.1;
+    cout << fl << endl;
+}
