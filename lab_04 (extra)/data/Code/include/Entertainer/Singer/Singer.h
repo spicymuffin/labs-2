057a97e8b08185be54624f1bccdbbd1944b74de3
spicymuffin <54279408+spicymuffin@users.noreply.github.com>
2023-12-05 10:34:46 +0900

    lab_04 data

---
+++ lab_04 (extra)/data/Code/include/Entertainer/Singer/Singer.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+#include "Entertainer/Artist.h"
+#include "Entertainer/Singer/Music.h"
+
+class Singer : public Artist {
+   public:
+	Singer(std::string);
+	virtual ~Singer();
+
+	Music getFavoriteSong();
+	void setFavoriteSong(Music);
+	std::string getJobName() override;
+
+   private:
+	Music _favoriteSong;
+};
